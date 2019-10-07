ba9cba416e3ae004fc785b0547c59665f392fe8b
Greg Rozon <grozon@ca.ibm.com>
2019-10-07 14:57:28 -0400

    Make change to test tagging

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,36 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Fib Calculator</h1>
+//           <Link to="/">Home</Link>
+//           <Link to="/otherpage">Other Page</Link>
+//         </header>
+//         <div>
+//           <Route exact path="/" component={Fib} />
+//           <Route path="/otherpage" component={OtherPage} />
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
 function App() {
   return (
-    <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fib Calculator</h1>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+          <p>Something to show</p>
         </div>
       </div>
-    </Router>
   );
 }
 
